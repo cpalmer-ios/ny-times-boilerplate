fb5431e26a89424874793bf6eed45252f8d6a16f
JulietteLaGuardia <juliettelaguardia@gmail.com>
2021-07-14 05:21:00 +0100

    added the fetch method for NYT articles

--- src/components/HooksArticlesContainer.js
+++ src/components/HooksArticlesContainer.js
@@ -3,7 +3,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchArticles } from "../redux";
 
 function HooksArticlesContainer() {
-  const data = useSelector((state) => [state.articles]);
+  const data = useSelector((state) => state.articles);
   const dispatch = useDispatch();
   return (
     <div>
@@ -12,12 +12,23 @@ function HooksArticlesContainer() {
       {data.loading ? (
         <h2>Loading...</h2>
       ) : data.error ? (
-        <h2>{data.results}</h2>
+        <h2>{data.error}</h2>
       ) : (
         <div>
           <h2>Content List</h2>
           <div>
-            {data.articles}
+            {data &&
+              data.articles &&
+              data.articles.map((article) => (
+                <div key={article.created_date}>
+                  <p>{article.section}</p>
+                  <p>{article.subsection}</p>
+                  <p>{article.title}</p>
+                  <p>{article.abstract}</p> 
+                  <p>{article.byline}</p>
+                  <img src={article.multimedia[0]}></img>
+                </div>
+              ))}
           </div>
         </div>
       )}
